bcb7aa095fa3558f8194242e3001a0cadc1fa3ea
mikelangelo1 <michael.oluwasola@stu.cu.edu.ng>
2021-07-03 05:34:24 +0100

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import TodoList from './components/TodoList';
 
 const App: React.FC =() => {
-  return <div className="App"></div>
+  return <div className="App">
+    <TodoList />
+  </div>
 }
 
 export default App;
